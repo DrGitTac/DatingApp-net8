1a525bcbf500cb82cea8a836b517e6d83ffaf511
DrGitTac <ecork96@gmail.com>
2025-01-25 14:29:01 -0600

    End of Section 8

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int ID { get; set; }
+    public required string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string? PublicID { get; set; }
+
+    // Navigation properties
+
+    public int AppUserId { get; set; }
+
+    public AppUser AppUser { get; set; } = null!; 
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,14 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,5 +20,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
